6722fe30d41d1caa7e92fdc4b587effd152ceb4d
Aleksandr Kononov <mister.San4es@list.ru>
2019-05-05 17:44:03 +0400

    blog.part1

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ . '/../functions.php';
+
+$routes = [
+    "/blog/public/" => 'functions/homepage.php',
+    "/blog/" => 'functions/homepage.php',
+    "/blog/public/about" => 'functions/about.php'
+
+//    "/" => 'functions/homepage.php',
+//    "/about" => 'functions/about.php'
+];
+$route = $_SERVER['REQUEST_URI'];
+if(array_key_exists($route, $routes)) {
+    include __DIR__ . '/../' . $routes[$route]; exit;
+} else {
+    dd(404);
+}
+
+
+
